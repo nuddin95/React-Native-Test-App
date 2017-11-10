b75781c3a73a25fa6a76b71db91c26571bdc6c49
Nadim Uddin <nadim.uddin1995@gmail.com>
2017-11-10 10:31:35 -0500

    updated styling of gallery

--- App.js
+++ App.js
@@ -4,17 +4,17 @@ import Component1 from './app/Components/Component1';
 import Component2 from './app/Components/Component2';
 import Component3 from './app/Components/Component3';
 import Component4 from './app/Components/Component4';
-import Component5 from './app/Components/Component5';
+import CameraComp from './app/Components/CameraComp';
 
 export default class myapp extends Component{
   render(){
     return (
       <View>
-        <Component1 message="Hello"/>
-        <Component2 />
-        <Component3 />
-        <Component4 />
-        <Component5 />
+        {/* <Component1 message="Hello"/> */}
+        {/* <Component2 /> */}
+        {/* <Component3 /> */}
+        {/* <Component4 /> */}
+        <CameraComp />
       </View>
       )
   }
---
+++ app/Components/CameraComp.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { 
+    AppRegistry, 
+    Dimensions, 
+    TouchableHighlight, 
+    Text, 
+    View, 
+    Platform, 
+    StyleSheet, 
+    Button,
+    Image} from 'react-native';
+import styles from './Styles.js'
+import Camera from 'react-native-camera';
+
+export default class CameraComp extends Component{
+    constructor(){
+        super();
+        this.state = {
+            camera:false,
+            pics:[],
+            currentImage:{}
+        }
+    }
+
+  render(){
+    return (
+        <View>
+            <Text>CAMERA STATE: {this.state.camera+""}</Text>
+            <Button
+                onPress={()=> this.setState({camera:!(this.state.camera)})}
+                title="Camera"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+            {this.state.camera ? (
+            <View style={styles.container}>
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={Camera.constants.Aspect.fill}>
+                    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                </Camera>
+            </View>):false}
+            {this.state.pics.length ? (
+                <View>
+                    <Text>GALLERY</Text>
+                    <View style = {styles.allImages}>
+                        {/* <Text>YOU'RE SUCH A GREAT PHOTOGRAPHER!!!</Text> */}
+                        {
+                            this.state.pics.map((pic, ind) => 
+                                (
+                                    <View key={ind}>
+                                        <Image
+                                            onClick={this.selectImage.bind(this)}
+                                            key={`pic #${ind}`}
+                                            style={styles.SingleImage}
+                                            source={{uri:pic.path}}
+                                        />
+                                    </View>
+                                )
+                            )
+                        }
+                    </View>
+                </View>):false
+            }
+                
+        </View>
+      )
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => {
+          console.log(data)
+          this.setState({pics:[...this.state.pics, data]})
+        })
+      .catch(err => console.error(err));
+  }
+
+  selectImage(){
+    console.log(this.key)
+    this.props.style = styles.SingleImagePressed;
+    this.setState();
+  }
+}
+
+AppRegistry.registerComponent('CameraComp', () => CameraComp);
